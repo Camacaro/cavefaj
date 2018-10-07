5b0bc90e1ee1468e7d088acadf8a41d64a998ac0
jcamacaro <jesus.alejandr@gmail.com>
2018-10-07 14:32:48 -0430

    seeder creados roles, permisos, usuarios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+    	DB::table('role_user')->truncate();
+        DB::table('permission_role')->truncate();
+        \App\Role::truncate();
+        \App\User::truncate();
+        \App\Permission::truncate(); 
+        $this->call(PermissionsSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+    	#Si es local se ejecutara esto
+        /*if (\Illuminate\Support\Facades\App::environment('local')) {
+
+        }*/
     }
 }
